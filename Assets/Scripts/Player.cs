55c7eb15069f1f7b2e57641d086f7267478514a7
theringsofsaturn <emiliankasemi@gmail.com>
2022-04-12 03:07:57 +0200

    Added Player movement accessing Rigidbody velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb; // Player's rigidbody
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //*  Assign the Rigidbody 
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //*  Horizontal input for left and right (x)
+        float x = Input.GetAxisRaw("Horizontal");
+
+        //*  Current velocity = new velocity (horizontal input, current velocity y)
+        rb.velocity = new Vector2(x, rb.velocity.y);
     }
+
 }
