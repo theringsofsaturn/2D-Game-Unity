2a0d3c4bed1ead476e0fb4841ab024c5890fb3d8
theringsofsaturn <emiliankasemi@gmail.com>
2022-04-13 02:54:02 +0200

    added LayerMask ground  layer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,27 +7,38 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb; // Player's rigidbody
     [SerializeField] private float jumpForce = 5f; // Jump force
     [SerializeField] private bool isGrounded = false; // Is player on the ground?
+    [SerializeField] private LayerMask groundLayer; // Layer mask for ground
 
     // Start is called before the first frame update
     void Start()
     {
-        //*  Assign the Rigidbody 
+        // Assign the Rigidbody 
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //*  Horizontal input for left and right (x)
+        //  Horizontal input for left and right (x)
         float move = Input.GetAxisRaw("Horizontal");
 
+        // If the player is on the ground and presses the spacebar, jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            //*  Jump
+            // Jump
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
         }
 
-        //*  Current velocity = new velocity (horizontal input, current velocity y)
+        // 2D Raycast to check if the player is on the ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
+
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+
+        // Current velocity = new velocity (horizontal input, current velocity y)
         rb.velocity = new Vector2(move, rb.velocity.y);
     }
 
